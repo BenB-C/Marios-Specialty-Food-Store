e1c2bb02c4587ec03a643cb445bb74b16a91eb77
bcauchon <bcauchon@gmail.com>
2019-08-09 13:57:08 -0700

    add ability to create a new review for a product

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @product.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "Could not save new review!"
+      render :new
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :rating, :content_body)
+    end
+end
