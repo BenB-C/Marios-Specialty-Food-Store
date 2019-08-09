597fbc7a4411ef965be2873513b7201d3c4ac8a8
bcauchon <bcauchon@gmail.com>
2019-08-09 14:17:03 -0700

    add three newest and most reviewed products to landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,7 @@
 class LandingPageController < ApplicationController
   def index
+    @new_products = Product.three_most_recent
+    @most_reviewed_product = Product.most_reviewed
     render :index
   end
 end
