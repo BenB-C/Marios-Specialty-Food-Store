d9b5700257943a74adb808db719bd8f2632202bc
bcauchon <bcauchon@gmail.com>
2019-08-09 12:19:39 -0700

    add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'landing_page/index'
+  root to: 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    resources :reviews
+  end
 end
