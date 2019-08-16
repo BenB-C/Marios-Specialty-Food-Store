45e6142aba6d490398807e4fe6a824469aca33cd
bcauchon <bcauchon@gmail.com>
2019-08-16 15:54:37 -0700

    add specs for products paths

--- spec/features/products_paths_spec.rb
+++ spec/features/products_paths_spec.rb
@@ -36,4 +36,70 @@ describe "#ProductsController" do
       expect(page).not_to have_content mexico_product.name
     end
   end
+
+  describe "add a product link" do
+    it "creates a new product" do
+      visit products_path
+      click_link "Add a product"
+      fill_in "Name", with: "Artichokes"
+      fill_in "Cost", with: "3.99"
+      fill_in "Country of origin", with: "Guatemala"
+      click_on "Create Product"
+      expect(page).to have_content "Artichokes"
+    end
+
+    it "displays an error message if an input is missing" do
+      visit products_path
+      click_link "Add a product"
+      click_on 'Create Product'
+      expect(page).to have_content "Could not save new product!"
+      expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content "Cost can't be blank"
+      expect(page).to have_content "Country of origin can't be blank"
+    end
+  end
+
+  describe "edit product link" do
+    it "allows a product to be edited" do
+      product = FactoryBot.create(:product)
+      visit product_path(product)
+      click_link "Edit product"
+      new_name = "Everlasting Gobstoppers"
+      new_cost = 99.99
+      new_country = "Loompaland"
+      fill_in "Name", with: new_name
+      fill_in "Cost", with: new_cost
+      fill_in "Country of origin", with: new_country
+      click_on "Update Product"
+      expect(page).to have_content new_name
+      expect(page).to have_content new_cost
+      expect(page).to have_content new_country
+    end
+
+    it "displays an error message if an input is missing" do
+      product = FactoryBot.create(:product)
+      visit product_path(product)
+      click_link "Edit product"
+      fill_in "Name", with: ""
+      fill_in "Cost", with: ""
+      fill_in "Country of origin", with: ""
+      click_on "Update Product"
+      expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content "Cost can't be blank"
+      expect(page).to have_content "Country of origin can't be blank"    end
+  end
+
+  describe "delete product link" do
+    it "deletes the product" do
+      product = FactoryBot.create(:product)
+      visit product_path(product)
+      click_link "Delete product"
+      click_link "NO"
+      expect(page).to have_content product.name
+      click_link "Delete product"
+      click_link "YES"
+      expect(page).not_to have_content product.name
+    end
+  end
+
 end
