b51b9afc26610f67004373e6f70aa829fc22ea42
bcauchon <bcauchon@gmail.com>
2019-08-16 09:59:19 -0700

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'landing_page/index'
-  root to: 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'landing_page#index'
+  get 'landing_page/index'
+  devise_for :users
+  resources :users
   resources :products do
     resources :reviews
   end
