285112bcadab06182e64f7820064c52bdde83581
bcauchon <bcauchon@gmail.com>
2019-08-16 14:04:40 -0700

    remove commented out line

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe "the root path" do
   it "shows a landing page" do
     visit root_path
-    # visit landing_page_index_path
     expect(page).to have_content "Mario's Specialty Food Products"
     expect(page).to have_content "New Products"
     expect(page).to have_content "Most Reviewed Product"
